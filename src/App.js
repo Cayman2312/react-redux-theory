7fcf9ed3a2cf3ad4bd0507f231e49ea53447d436
M.Studitskiy <cayman2312@gmail.com>
2020-04-23 11:18:20 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.scss';
+
+class App extends Component {
+  updateCounter(value) {
+    // this.setState({
+    //   counter: this.props.counter + value,
+    // });
+  }
+
+  render() {
+    console.log('this.props', this.props);
+    return (
+      <div className={'App'}>
+        <h1>
+          Счетчик <strong>{this.props.counter}</strong>
+        </h1>
+
+        <hr />
+
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
+          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    counter: state.counter,
+  };
+}
+
+export default connect(mapStateToProps)(App);
